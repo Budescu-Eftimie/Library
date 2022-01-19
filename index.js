32f3d09c15bd66071510c5e6e68956bb31bfdca0
Budescu Eftimie <budescueftimie@gmail.com>
2022-01-19 05:00:52 +0100

    done logic for addBook-btn, bookForm, libraryTable

--- index.js
+++ index.js
@@ -1,50 +1,88 @@
 "use strict";
 
+
+
+const addBookbtn = document.querySelector(".add-book");
+const form = document.getElementsByClassName("wrapper")[0];
+
+const library = [];
+
+
+addBookbtn.onclick = () => {
+  form.classList.remove("hidden");
+};
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  makeBook();
+  //displayBooks();
+};
+
+const makeBook = () => {
+  const book = new Book();
+  book.title = document.getElementById("title").value;
+  book.author = document.getElementById("author").value;
+  book.pages = document.getElementById("pages").value;
+  book.isRead = document.getElementById("done").checked;
+  const isBookInLibrary = bookCheck(book);
+  console.log(isBookInLibrary);
+
+  if (!isBookInLibrary) {
+    library.push(book);
+    book.displayBook();
+  } else {
+    const alertMessage = document.getElementsByClassName("title-card")[0];
+    alertMessage.innerText = "Book already in library";
+    alertMessage.style.color = "red";
+  }
+
+};
+
+const bookCheck = (book) => {
+  for (let index = 0; index < library.length; index++) {
+    const element = library[index].title;
+    console.log(book.title, "-", element);
+    if (book.title === element) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
 }
+Book.prototype.displayBook = function () {
+  const bookCollection = document.getElementsByClassName("table-body")[0];
+  const bookInfo = document.createElement("tr");
+  bookInfo.className = "book-info";
+  bookCollection.appendChild(bookInfo);
 
-let makeBook = () => {
-  let books = [];
-  const book = Object.create(Book);
-  book.title = document.getElementById("title").value;
-  book.author = document.getElementById("author").value;
-  book.pages = document.getElementById("pages").value;
-  book.isRead = document.getElementById("done").checked;
-  let appendBook = () => {
-    books.push(book);
-    console.log(book,books);
-  };
-  return appendBook;
+  const domTitle = document.createElement("td");
+  domTitle.innerText = `${this.title}`;
+
+  const domAuthor = document.createElement("td");
+  domAuthor.innerText = `${this.author}`;
+
+  const domPages = document.createElement("td");
+  domPages.innerText = `${this.pages}`;
+
+  const domStatus = document.createElement("td");
+  if (this.isRead == true) {
+    domStatus.className = "fas fa-check";
+  } else {
+    domStatus.className = "fas fa-times";
+  }
+
+  const domRemove = document.createElement("td");
+  domRemove.className = "far fa-trash-alt";
+
+  let domElements = [domRemove, domTitle, domAuthor, domPages, domStatus];
+  domElements.forEach((element) => {
+    bookInfo.appendChild(element);
+  });
 };
-
-const main = () => {
-  const addBookbtn = document.querySelector(".add-book");
-  const form = document.getElementsByClassName("wrapper")[0];
-
-  addBookbtn.onclick = () => {
-    form.classList.remove("hidden");
-  };
-
-  form.onsubmit = (event) => {
-    event.preventDefault();
-    let books = makeBook();
-    console.log(books());
-  };
-};
-
-main();
-
-// const addBookbtn = document.querySelector(".add-book");
-
-// let form = document.getElementsByClassName("wrapper")[0];
-
-// form.addEventListener('submit'; function (event) {
-
-// 	// Prevent form from submitting to the server
-// 	event.preventDefault();
-//       let data = Object.fromEntries(new FormData(event.target).entries())
-//       console.log(data)
